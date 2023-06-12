99e97f8ebffb39373735e31ec51b3e73fc7af9f4
Mohammad Sami Usmani <mohammad.sami93@outlook.com>
2023-06-13 00:33:18 +0500

    verified fasion mnist data successfully

--- verify.py
+++ verify.py
@@ -4,6 +4,38 @@ import torch.nn as nn
 import torch.nn.functional as tf
 import matplotlib.pyplot as plt
 import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+
+batch_size = 10
+
+# Class labels
+classes = ('T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+        'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot')
+
+#transform all images to same size
+transform = tv.transforms.Compose(
+    [tv.transforms.ToTensor(),
+    tv.transforms.Normalize((0.5,), (0.5,))])
+
+# functions to show an image
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+validation_set = tv.datasets.FashionMNIST('./data', train=False, transform=transform, download=True)
+validation_loader = t.utils.data.DataLoader(validation_set, batch_size, shuffle=False)
+
+# get some random training images
+dataiter = iter(validation_loader)
+images, labels = next(dataiter)
+
+# print labels
+print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
+# show images
+imshow(tv.utils.make_grid(images))
 
 # PyTorch models inherit from torch.nn.Module
 class GarmentClassifier(nn.Module):
@@ -28,3 +60,10 @@ class GarmentClassifier(nn.Module):
 path = './final'
 saved_model = GarmentClassifier()
 saved_model.load_state_dict(t.load(path))
+saved_model.eval()
+
+outputs = saved_model(images)
+
+_, predicted = t.max(outputs, 1)
+
+print('Predicted: ', ' '.join(f'{classes[predicted[j]]:5s}' for j in range(batch_size)))
